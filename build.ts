ce918523fc49b96a30198838135d234d4618c9b2
developer-velo <69754644+developer-velo@users.noreply.github.com>
2026-04-01 15:18:37 +0200

    build: configure build pipeline
    Configure build process using Bun/Bundler and TypeScript.

---
+++ build.ts
@@ -0,0 +1,20 @@
+import type { BuildConfig } from "bun";
+
+const config: BuildConfig = {
+  entrypoints: ["index.ts"],
+  target: "node",
+  outdir: "output",
+  sourcemap: "external"
+};
+
+await Bun.build({
+  ...config,
+  format: "cjs",
+  naming: "[name].cjs"
+})
+
+await Bun.build({
+  ...config,
+  format: "esm",
+  naming: "[name].mjs"
+});
