479c1110514f27538bea16525e1a2623ca5e0074
developer-velo <69754644+developer-velo@users.noreply.github.com>
2026-04-01 18:36:32 +0200

    feat: implement core structures and constructor logic
    Define core data structures (Model, Options, Resolution) and write constructor logic with resolution and options merging.

--- matrix.ts
+++ matrix.ts
@@ -1,7 +1,35 @@
-interface Model { }
+type Resolution = 16 | 32 | 64 | 128 | 256 | 512;
+
+interface Model {
+  options: Options;
+  resolution: Resolution;
+}
+
+interface Options {
+  size: number;
+  density: number;
+  padding: number;
+  threshold: number;
+}
+
+const defaultOptions: Options = {
+  size: 2.0,
+  density: 0.5,
+  padding: 2.0,
+  threshold: 80.0,
+};
 
 class Matrix implements Model {
-  public constructor() { }
+  public options: Options;
+  public resolution: Resolution;
+
+  public constructor(
+    resolution: Resolution,
+    options: Partial<Options> = defaultOptions,
+  ) {
+    this.options = { ...defaultOptions, ...options };
+    this.resolution = resolution;
+  }
 }
 
 export default Matrix;
